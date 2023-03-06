42ab848e0b6dbe338000e13707a7b2fae446dcbb
Rosen Ivanov <ross.ivanov88@gmail.com>
2023-03-06 18:51:16 +0000

    addin SquareArea and InchesToCentimeters

---
+++ SoftUniBasics/FirstSteps-Lab/InchesToCentimeters.js
@@ -0,0 +1,9 @@
+function inchesToCentimeters(input) {
+    let inch = Number(input[0])
+    let inchToCm = 2.54
+
+    console.log(inch*inchToCm);
+}
+
+inchesToCentimeters(["5"]);
+inchesToCentimeters(["7"]);
---
+++ SoftUniBasics/FirstSteps-Lab/SquareArea.js
@@ -0,0 +1,7 @@
+function squareArea(input) {
+
+    let side = Number(input[0]);
+    let areaResult =  side * side;
+    console.log(areaResult);
+}
+squareArea(["5"]);
