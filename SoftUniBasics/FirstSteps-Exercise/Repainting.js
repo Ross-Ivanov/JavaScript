880ad264ade3db3863d0b9c1b50318fd10affdbd
Rosen Ivanov <ross.ivanov88@gmail.com>
2023-03-08 18:43:42 +0000

    adding the input for required materials

--- SoftUniBasics/FirstSteps-Exercise/Repainting.js
+++ SoftUniBasics/FirstSteps-Exercise/Repainting.js
@@ -4,6 +4,11 @@ function repainting(input) {
     let paintPricePerLiter = 14.50;
     let painThinnerPricePerLiter = 5.00;
 
+    let nylonRequired = Number(input[0])
+    let paintRequired = Number(input[1])
+    let thinnerRequired = Number(input[2])
+    let hoursRequired = Number(input[3])
+    
 }
 repainting(["10 ", "11 ", "4 ", "8 "]);
 repainting(["5 ", "10 ", "10 ", "1 "]);
