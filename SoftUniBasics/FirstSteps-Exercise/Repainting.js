49eeed6a1dc37eeac8966df819697b112d3f633b
Rosen Ivanov <ross.ivanov88@gmail.com>
2023-03-08 18:37:53 +0000

    adding prices

--- SoftUniBasics/FirstSteps-Exercise/Repainting.js
+++ SoftUniBasics/FirstSteps-Exercise/Repainting.js
@@ -1,5 +1,8 @@
 function repainting(input) {
-
+    
+    let nylonPricePerSqMeter = 1.50;
+    let paintPricePerLiter = 14.50;
+    let painThinnerPricePerLiter = 5.00;
 
 }
 repainting(["10 ", "11 ", "4 ", "8 "]);
