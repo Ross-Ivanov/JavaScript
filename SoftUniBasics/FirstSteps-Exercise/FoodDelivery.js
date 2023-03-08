51214733164ff19488c759a47042ec4c4edba83e
Rosen Ivanov <ross.ivanov88@gmail.com>
2023-03-08 19:20:35 +0000

    adding solution for FoodDelivery

--- SoftUniBasics/FirstSteps-Exercise/FoodDelivery.js
+++ SoftUniBasics/FirstSteps-Exercise/FoodDelivery.js
@@ -1,6 +1,17 @@
 function foodDelivery(input) {
 
+    let chickenMeal = 10.35;
+    let fishMeal = 12.40;
+    let veggiMeal = 8.15;
+    let deliveryPrice = 2.50
 
+    let chickenMealCount = Number (input[0]);
+    let fishMealCount = Number (input[1]);
+    let veggiMealCount = Number (input[2]);
+
+    let finalBill = ((chickenMealCount * chickenMeal + fishMealCount * fishMeal + veggiMealCount * veggiMeal) * 1.2)+ deliveryPrice
+    
+    console.log(finalBill);
 }
 
 foodDelivery(["2 ", "4 ", "3 "]);
