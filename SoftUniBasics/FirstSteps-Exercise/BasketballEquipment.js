43f78aed7e37c150a30a1b00590a8c4e84a8ed9f
Rosen Ivanov <ross.ivanov88@gmail.com>
2023-03-09 19:23:43 +0000

    addin solution to Basketball Equipment task

--- SoftUniBasics/FirstSteps-Exercise/BasketballEquipment.js
+++ SoftUniBasics/FirstSteps-Exercise/BasketballEquipment.js
@@ -1,8 +1,15 @@
 function basketballEquipment(input) {
 
     let taxPerYear = Number (input[0]);
-    
 
+    let shoesPrice = taxPerYear * 0.6 ;
+    let jerseyPrice = shoesPrice * 0.8;
+    let ballPrice = jerseyPrice * (1/4);
+    let accessoriesPrice = ballPrice * (1/5)
+
+    let totalExpenses = taxPerYear + shoesPrice + jerseyPrice + ballPrice + accessoriesPrice
+
+    console.log(totalExpenses);
 
 }
 
