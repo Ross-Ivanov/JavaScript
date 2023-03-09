06ac91a896a7b2d1d25556c285b2868b3c34c3d4
Rosen Ivanov <ross.ivanov88@gmail.com>
2023-03-09 19:32:50 +0000

    adding fish tank solution

--- SoftUniBasics/FirstSteps-Exercise/FishTank.js
+++ SoftUniBasics/FirstSteps-Exercise/FishTank.js
@@ -1,6 +1,14 @@
 function fishTank(input) {
 
+    let lenght = Number (input[0]);
+    let width = Number (input[1]);
+    let height = Number (input[2]);
+    let percentFilled = Number (input[3])/100;
 
+    let fishTankVolumeInLiters = (lenght * width * height)/1000;
+    let totalLitersNeeded = fishTankVolumeInLiters * (1-percentFilled);
+
+    console.log(totalLitersNeeded);
 }
 
 fishTank(["85 ", "75 ", "47 ", "17 "]);
