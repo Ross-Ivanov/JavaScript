8a91b7b8cfb8d49fd7b013a3286274120cb6d0cd
Rosen Ivanov <ross.ivanov88@gmail.com>
2023-03-13 22:06:19 +0000

    adding solution for Swiming record

--- SoftUniBasics/ConditionalStatements-Exercise/WorldSwimmingRecord.js
+++ SoftUniBasics/ConditionalStatements-Exercise/WorldSwimmingRecord.js
@@ -3,8 +3,15 @@ function worldSwimmingRecord(input) {
     let currentRecord = Number(input[0]);
     let distanceInMeters = Number(input[1]);
     let secondsPerMeter = Number(input[2]);
-    
 
+    let timeDelay = Math.floor(distanceInMeters / 15) *12.5;
+    let totalSecondsForTheDistance = (distanceInMeters * secondsPerMeter) + timeDelay
+
+    if (totalSecondsForTheDistance < currentRecord){
+        console.log(`Yes, he succeeded! The new world record is ${totalSecondsForTheDistance.toFixed(2)} seconds.`);
+    } else {
+        console.log(`No, he failed! He was ${Math.abs(currentRecord - totalSecondsForTheDistance).toFixed(2)} seconds slower.`);
+    }
 }
 
 worldSwimmingRecord(["10464", "1500", "20"]);
