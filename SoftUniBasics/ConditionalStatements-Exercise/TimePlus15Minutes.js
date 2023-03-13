08726c21823ad4ffdc0bd5c0cef101038baf26e3
Rosen Ivanov <ross.ivanov88@gmail.com>
2023-03-13 05:23:41 +0000

    adding files without solutions

--- SoftUniBasics/ConditionalStatements-Exercise/TimePlus15Minutes.js
+++ SoftUniBasics/ConditionalStatements-Exercise/TimePlus15Minutes.js
@@ -1,4 +1,23 @@
 function timePlus15Minutes(input) {
 
+    let hour = Number(input[0]);
+    let minutes = Number(input[1]);
 
+    let totalMinutes = hour *60 + minutes +15;
+
+    if (Math.floor(totalMinutes / 60) === 24){
+        hour = 0;
+    } else {
+        hour = Math.floor(totalMinutes / 60);
+    }
+
+    minutes = ((totalMinutes % 60).toString()).padStart(2, 0);
+    
+    console.log(`${hour}:${minutes}`);
 }
+
+timePlus15Minutes(["1", "46"]);
+timePlus15Minutes(["0", "01"]);
+timePlus15Minutes(["23", "59"]);
+timePlus15Minutes(["11", "08"]);
+timePlus15Minutes(["12", "49"]);
