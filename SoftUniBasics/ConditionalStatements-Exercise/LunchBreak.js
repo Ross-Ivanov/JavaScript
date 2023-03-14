dc5a2eb215a57785e4fec419d27b23913fa46fa1
Rosen Ivanov <ross.ivanov88@gmail.com>
2023-03-14 05:57:49 +0000

    adding testcase and input for LunchBreak

--- SoftUniBasics/ConditionalStatements-Exercise/LunchBreak.js
+++ SoftUniBasics/ConditionalStatements-Exercise/LunchBreak.js
@@ -1,3 +1,10 @@
 function lunchBreak(input) {
 
+    let tvSeriesName = input[0];
+    let episodeLenght = Number(input[1]);
+    let breakLenght = Number(input[2]);
+    
 }
+
+lunchBreak(["Game of Thrones", "60", "96"]);
+lunchBreak(["Teen Wolf", "48", "60"]);
