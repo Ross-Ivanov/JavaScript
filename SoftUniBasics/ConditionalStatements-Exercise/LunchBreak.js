c2a090859092c6cda894df13525ed377c3f746a8
Rosen Ivanov <ross.ivanov88@gmail.com>
2023-03-15 20:06:06 +0000

    adding solution for lunch break

--- SoftUniBasics/ConditionalStatements-Exercise/LunchBreak.js
+++ SoftUniBasics/ConditionalStatements-Exercise/LunchBreak.js
@@ -3,7 +3,18 @@ function lunchBreak(input) {
     let tvSeriesName = input[0];
     let episodeLenght = Number(input[1]);
     let breakLenght = Number(input[2]);
-    
+
+    let luchTime = breakLenght * (1/8);
+    let restTime = breakLenght * (1/4);
+
+    let timeLeftOrNeeded = breakLenght - (luchTime + restTime + episodeLenght)
+
+    if (timeLeftOrNeeded >= 0) {
+        console.log(`You have enough time to watch ${tvSeriesName} and left with ${Math.ceil(timeLeftOrNeeded)} minutes free time.`);
+    } else {
+        console.log(`You don't have enough time to watch ${tvSeriesName}, you need ${Math.ceil(Math.abs(timeLeftOrNeeded))} more minutes.`);
+    }
+
 }
 
 lunchBreak(["Game of Thrones", "60", "96"]);
