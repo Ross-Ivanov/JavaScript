14a96ce9bef6437b55141b3d83341eb36bfef342
Rosen Ivanov <ross.ivanov88@gmail.com>
2023-03-16 21:04:47 +0000

    adding test cases

--- SoftUniBasics/ConditionalStatementsAdvanced-Lab/TradeCommissions.js
+++ SoftUniBasics/ConditionalStatementsAdvanced-Lab/TradeCommissions.js
@@ -1,3 +1,8 @@
 function tradeCommissions(input) {
 
 }
+
+tradeCommissions(["Sofia", "1500"]);
+tradeCommissions(["Plovdiv", "499.99"]);
+tradeCommissions(["Varna", "3874.50"]);
+tradeCommissions(["Kaspichan", "-50"]);
