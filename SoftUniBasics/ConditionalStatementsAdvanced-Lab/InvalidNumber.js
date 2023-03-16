1384801943a2e0a57fecc63ce425c5785e60ae01
Rosen Ivanov <ross.ivanov88@gmail.com>
2023-03-16 21:01:13 +0000

    adding test cases

--- SoftUniBasics/ConditionalStatementsAdvanced-Lab/InvalidNumber.js
+++ SoftUniBasics/ConditionalStatementsAdvanced-Lab/InvalidNumber.js
@@ -1,3 +1,12 @@
 function invalidNumber(input) {
     
 }
+
+invalidNumber(["75"]);
+invalidNumber(["150"]);
+invalidNumber(["220"]);
+invalidNumber(["199"]);
+invalidNumber(["-1"]);
+invalidNumber(["100"]);
+invalidNumber(["200"]);
+invalidNumber(["0"]);
