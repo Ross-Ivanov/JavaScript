ae531e8f26c210f90a88b902cf6af11bbcc94dbc
Rosen Ivanov <ross.ivanov88@gmail.com>
2023-03-16 21:03:08 +0000

    adding test cases

--- SoftUniBasics/ConditionalStatementsAdvanced-Lab/FruitShop.js
+++ SoftUniBasics/ConditionalStatementsAdvanced-Lab/FruitShop.js
@@ -1,3 +1,9 @@
 function fruitShop(input) {
 
 }
+
+fruitShop(["apple", "Tuesday", "2"]);
+fruitShop(["orange", "Sunday", "3"]);
+fruitShop(["kiwi", "Monday", "2.5"]);
+fruitShop(["grapes", "Saturday", "0.5"]);
+fruitShop(["tomato", "Monday", "0.5"]);
