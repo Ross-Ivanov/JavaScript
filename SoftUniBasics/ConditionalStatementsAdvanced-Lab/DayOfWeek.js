17897f271ecffae00610040775405e2d4fd3730f
Rosen Ivanov <ross.ivanov88@gmail.com>
2023-03-16 20:28:19 +0000

    adding test cases

--- SoftUniBasics/ConditionalStatementsAdvanced-Lab/DayOfWeek.js
+++ SoftUniBasics/ConditionalStatementsAdvanced-Lab/DayOfWeek.js
@@ -1,3 +1,12 @@
 function dayOfWeek(input) {
 
 }
+
+dayOfWeek(["1"]);
+dayOfWeek(["2"]);
+dayOfWeek(["3"]);
+dayOfWeek(["4"]);
+dayOfWeek(["5"]);
+dayOfWeek(["6"]);
+dayOfWeek(["7"]);
+dayOfWeek(["-1"]);
