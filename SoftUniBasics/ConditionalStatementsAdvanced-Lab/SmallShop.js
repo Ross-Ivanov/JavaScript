ccaddcd5c79b91a5b8cb36d13b9f3a55a10c6b66
Rosen Ivanov <ross.ivanov88@gmail.com>
2023-03-16 20:39:38 +0000

    adding test cases

--- SoftUniBasics/ConditionalStatementsAdvanced-Lab/SmallShop.js
+++ SoftUniBasics/ConditionalStatementsAdvanced-Lab/SmallShop.js
@@ -1,3 +1,9 @@
 function smallShop(input) {
 
 }
+
+smallShop(["coffee", "Varna", "2"]);
+smallShop(["peanuts", "Plovdiv", "1"]);
+smallShop(["beer", "Sofia", "2"]);
+smallShop(["water", "Plovdiv", "2"]);
+smallShop(["sweets", "Sofia", "2.23"]);
