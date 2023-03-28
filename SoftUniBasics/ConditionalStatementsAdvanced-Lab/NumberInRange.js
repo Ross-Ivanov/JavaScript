657a65899b2e2d449dacd816080ecc652ff40d6d
Rosen Ivanov <ross.ivanov88@gmail.com>
2023-03-28 18:57:47 +0100

    adding solution

--- SoftUniBasics/ConditionalStatementsAdvanced-Lab/NumberInRange.js
+++ SoftUniBasics/ConditionalStatementsAdvanced-Lab/NumberInRange.js
@@ -1,5 +1,11 @@
 function numberInRange(input) {
+    let numberToCheck = Number(input[0]);
 
+    if (numberToCheck>= -100 && numberToCheck <=100 && numberToCheck != 0){
+        console.log("Yes");
+    } else {
+        console.log("No");
+    }
 }
 
 numberInRange(["-25"]);
