7cd415fcd0882336ec3437a84740356d3cae30aa
Rosen Ivanov <ross.ivanov88@gmail.com>
2023-04-16 17:26:59 +0100

    adding testcase

--- SoftUniBasics/ConditionalStatementsAdvanced-Exercise/OperationsBetweenNumbers.js
+++ SoftUniBasics/ConditionalStatementsAdvanced-Exercise/OperationsBetweenNumbers.js
@@ -1,3 +1,11 @@
 function operationsBetweenNumbers(input) {
 
 }
+
+operationsBetweenNumbers(["10", "12", "+"]);
+operationsBetweenNumbers(["123", "12", "/"]);
+operationsBetweenNumbers(["112", "0", "/"]);
+operationsBetweenNumbers(["10", "1", "-"]);
+operationsBetweenNumbers(["10", "3", "%"]);
+operationsBetweenNumbers(["10", "0", "%"]);
+operationsBetweenNumbers(["7", "3", "*"]);
